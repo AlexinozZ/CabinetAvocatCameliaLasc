1aff3926e06cf3ececb4d7e562600b0e3db85644
AlexinozZ <32441040+AlexinozZ@users.noreply.github.com>
2025-09-23 14:07:25 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import PracticeAreas from './components/PracticeAreas';
@@ -58,7 +57,6 @@ function App() {
   
   return (
     <div className="min-h-screen">
-      <Header />
       <Hero onOpenConsultationModal={openConsultationModal} />
       <About id="about" />
       <PracticeAreas id="practice-areas" />
