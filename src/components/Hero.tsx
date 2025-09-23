56ebef0f6b81676c493ce0af39c5538dcb9bb9d3
AlexinozZ <32441040+AlexinozZ@users.noreply.github.com>
2025-09-23 14:55:48 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero: React.FC<HeroProps> = ({ onOpenConsultationModal }) => {
           {/* Main Logo */}
           <div className="mb-6">
             <img 
-              src="/logo-transparent.svg" 
+              src="/logo-gradient.svg" 
               alt="Cabinet Avocat Lasc Camelia Irina Logo" 
               className="w-64 h-64 sm:w-80 sm:h-80 object-contain"
             />
